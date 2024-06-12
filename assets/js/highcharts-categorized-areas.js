1f4c553957e23925896234a7eb8d7acebaf89402
Philip Boden <philip.boden@gmail.com>
2024-06-12 01:50:23 -0700

    Development of example. Styled like the figma example. Work related to getting control over the sankey chart output

--- assets/js/highcharts-categorized-areas.js
+++ assets/js/highcharts-categorized-areas.js
@@ -4,164 +4,183 @@
         'https://code.highcharts.com/mapdata/countries/us/us-ca-all.topo.json'
     ).then(response => response.json());
 
-
     const labels = ['column 2', 'column 1', 'column 3'];
 
-// Create the chart
+    // Create the map of California.
     let map = Highcharts.mapChart('map', {
-        chart: {
-            map: topology,
-            spacingBottom: 20,
+
+      title: {
+        text: '',
+      },
+
+      subtitle: {
+        text: '',
+      },
+
+      chart: {
+        map: topology,
+        backgroundColor: 'none',
+        events: {
+          // load default Sankey chart
+          load: function () {
+            sankeyChart.update({
+              // title: {
+              //   text: this.series.name ?? 'Data',
+              // },
+              // series: [{
+              //   data: sankeyChart.series[0].data.visible
+              // }]
+            })
+          }
+        },
+      },
+
+      accessibility: {
+        series: {
+          descriptionFormat: 'Timezone {series.name} with ' + '{series.points.length} countries.'
+        },
+        point: {
+          valueDescriptionFormat: '{point.name}.'
+        }
+      },
+
+      legend: {
+        enabled: false,
+      },
+
+      plotOptions: {
+        map: {
+          point: {
             events: {
 
-                // load default Sankey chart
-                load: function () {
-                    sankeyChart.update({
-                        title: {
-                            text: this.series.name
-                        },
-                        series: [{
-                            // data: sankeyData('default')
-                        }]
-                    })
-                }
+              mouseOver: function () {
+                map.series.forEach(s => {
+                  s.points.forEach((point) => {
+                    point.setState('hover');
+                  })
+                })
+              },
+
+              mouseOut: function () {
+                map.series.forEach(s => {
+                  s.points.forEach((point) => {
+                    point.setState('');
+                  })
+                })
+              },
+
+              click: function () {
+                // map.setTitle({text: 'Ninth Grade to Postsecondary Award: ' + this.series.name})
+
+                // map.series.forEach(s => {
+                //   s.points.forEach((point) => {
+                //     point.select(false);
+                //   });
+                // });
+
+                Highcharts.each(this.series.points, function (p) {
+                  p.select(true, true);
+                });
+
+                // update sankey chart based on group.
+                sankeyChart.update({
+                  title: {
+                    text: this.series.name ?? null,
+                  },
+                  series: [{
+                    //data: sankeyData('default')
+                  }]
+                })
+              },
             },
-        },
+          },
 
-        title: {
-            text: 'Ninth Grade to Postsecondary Award'
-        },
+          allAreas: false,
 
-        accessibility: {
-            series: {
-                descriptionFormat: 'Timezone {series.name} with ' +
-                    '{series.points.length} countries.'
+          joinBy: ['hc-key', 'code'],
+
+          dataLabels: {
+            enabled: false,
+          },
+
+          tooltip: {
+            headerFormat: '',
+            pointFormat: '<div>{point.name}: <b>{series.name}</b></div>'
+          },
+
+          states: {
+            select: {
+              color: 'black',
+              lineWidth: 10,
+              lineColor: 'black',
+              fillColor: 'black',
             },
-            point: {
-                valueDescriptionFormat: '{point.name}.'
-            }
+            hover: {}
+          }
         },
+      },
 
-        legend: {
-            enabled: false
+      credits: {
+        enabled: false,
+      },
+
+      caption: {
+        text: '',
+      },
+
+      series: [
+        {
+          name: 'Group 1',
+          data: ['us-ca-015', 'us-ca-023', 'us-ca-045', 'us-ca-033', 'us-ca-097', 'us-ca-041', 'us-ca-055', 'us-ca-095', 'us-ca-013', 'us-ca-001', 'us-ca-075', 'us-ca-081', 'us-ca-085',
+          ].map(code => ({
+              code
+          }))
+        }, {
+          name: 'Group 2',
+          data: ['us-ca-105', 'us-ca-103', 'us-ca-021', 'us-ca-007', 'us-ca-011',].map(code => ({
+              code
+          }))
+        }, {
+          name: 'Group 3',
+          data: ['us-ca-093', 'us-ca-049', 'us-ca-089', 'us-ca-035', 'us-ca-063', 'us-ca-091', 'us-ca-057',].map(code => ({
+              code
+          }))
+        }, {
+          name: 'Group 4',
+          data: ['us-ca-115', 'us-ca-101', 'us-ca-061', 'us-ca-017', 'us-ca-067', 'us-ca-113',].map(code => ({
+              code
+          }))
+        }, {
+          name: 'Group 5',
+          data: ['us-ca-077', 'us-ca-099', 'us-ca-047', 'us-ca-039', 'us-ca-019', 'us-ca-031', 'us-ca-107', 'us-ca-029'].map(code => ({
+              code
+          }))
+        }, {
+          name: 'Group 6',
+          color: 'green',
+          points: {
+              fillColor: 'red',
+              lineWidth: 5
+          },
+          data: ['us-ca-005', 'us-ca-003', 'us-ca-009', 'us-ca-109', 'us-ca-043', 'us-ca-051', 'us-ca-027',].map(code => ({
+              code
+          }))
+        }, {
+          name: 'Group 7',
+          data: ['us-ca-071', 'us-ca-065',].map(code => ({
+              code
+          }))
+        }, {
+          name: 'Group 8',
+          data: ['us-ca-059', 'us-ca-073', 'us-ca-025',].map(code => ({
+              code
+          }))
+        }, {
+          name: 'Group 9',
+          data: ['us-ca-087', 'us-ca-069', 'us-ca-053', 'us-ca-079', 'us-ca-083', 'us-ca-111', 'us-ca-037',].map(code => ({
+              code
+          }))
         },
-
-        plotOptions: {
-            map: {
-                point: {
-                    events: {
-                        mouseOver: function () {
-                            map.series.forEach(s => {
-                                s.points.forEach((point) => {
-                                    point.setState('hover');
-                                })
-                            })
-                        },
-                        mouseOut: function () {
-                            map.series.forEach(s => {
-                                s.points.forEach((point) => {
-                                    point.setState('');
-                                })
-                            })
-                        },
-                        click: function () {
-                            map.setTitle({text: 'Ninth Grade to Postsecondary Award: ' + this.series.name})
-                            map.series.forEach(s => {
-                                s.points.forEach((point) => {
-                                    point.select(false);
-                                });
-                            });
-
-                            Highcharts.each(this.series.points, function (p) {
-                                p.select(true, true);
-                            });
-
-                            // update sankey chart based on group.
-                            sankeyChart.update({
-                                title: {
-                                    text: this.series.name
-                                },
-                                series: [{
-                                    // data: sankeyData(this.series.name)
-                                    // data: data['default']
-                                }]
-                            })
-                        },
-                    },
-                },
-                allAreas: false,
-                joinBy: ['hc-key', 'code'],
-                dataLabels: {
-                    enabled: false,
-                },
-                tooltip: {
-                    headerFormat: '',
-                    pointFormat: '{point.name}: <b>{series.name}</b>'
-                },
-                states: {
-                    select: {
-                        color: 'black',
-                        lineWidth: 10,
-                        lineColor: 'black',
-                        fillColor: 'black',
-                    }
-                }
-            },
-        },
-        series: [
-            {
-                name: 'Group 1',
-                data: ['us-ca-015', 'us-ca-023', 'us-ca-045', 'us-ca-033', 'us-ca-097', 'us-ca-041',
-                    'us-ca-055', 'us-ca-095', 'us-ca-013', 'us-ca-001', 'us-ca-075', 'us-ca-081', 'us-ca-085',
-                ].map(code => ({
-                    code
-                }))
-            }, {
-                name: 'Group 2',
-                data: ['us-ca-105', 'us-ca-103', 'us-ca-021', 'us-ca-007', 'us-ca-011',].map(code => ({
-                    code
-                }))
-            }, {
-                name: 'Group 3',
-                data: ['us-ca-093', 'us-ca-049', 'us-ca-089', 'us-ca-035', 'us-ca-063', 'us-ca-091', 'us-ca-057',].map(code => ({
-                    code
-                }))
-            }, {
-                name: 'Group 4',
-                data: ['us-ca-115', 'us-ca-101', 'us-ca-061', 'us-ca-017', 'us-ca-067', 'us-ca-113',].map(code => ({
-                    code
-                }))
-            }, {
-                name: 'Group 5',
-                data: ['us-ca-077', 'us-ca-099', 'us-ca-047', 'us-ca-039', 'us-ca-019', 'us-ca-031', 'us-ca-107', 'us-ca-029'].map(code => ({
-                    code
-                }))
-            }, {
-                name: 'Group 6',
-                color: 'green',
-                points: {
-                    fillColor: 'red',
-                    lineWidth: 5
-                },
-                data: ['us-ca-005', 'us-ca-003', 'us-ca-009', 'us-ca-109', 'us-ca-043', 'us-ca-051', 'us-ca-027',].map(code => ({
-                    code
-                }))
-            }, {
-                name: 'Group 7',
-                data: ['us-ca-071', 'us-ca-065',].map(code => ({
-                    code
-                }))
-            }, {
-                name: 'Group 8',
-                data: ['us-ca-059', 'us-ca-073', 'us-ca-025',].map(code => ({
-                    code
-                }))
-            }, {
-                name: 'Group 9',
-                data: ['us-ca-087', 'us-ca-069', 'us-ca-053', 'us-ca-079', 'us-ca-083', 'us-ca-111', 'us-ca-037',].map(code => ({
-                    code
-                }))
-            },
-        ],
+      ],
     });
 })();
--- assets/js/highcharts-sankey-diagram.js
+++ assets/js/highcharts-sankey-diagram.js
@@ -1,129 +1,238 @@
 const nodes = [
     {
         id: 'Ninth Grade',
-        // color: '#ffa500',
-        offsetHorizontal: -50,
-        offsetVertical: 10
+        column: 1,
+        offset: -100,
+        color: '#ffa500',
+        marginTop: 0,
+        offsetVertical: -100,
     },
     {
         id: 'Completed High School',
-        // color: '#74ffe7',
-        offsetHorizontal: -50,
-        offsetVertical: 130
+        column: 2,
+        color: '#74ffe7',
     },
-    // {
-    //     id: 'Did Not Complete High School',
-    //     color: '#8cff74',
-    //     offset: -110
-    // },
-    // {
-    //     id: 'Four-year college',
-    //     color: '#ff8da1',
-    //     column: 3,
-    //     offset: 50
-    // },
-    // {
-    //     id: 'Two-year community college',
-    //     color: '#f4c0ff',
-    //     column: 3,
-    //     offset: 50
-    // },
-    // {
-    //     id: 'Never enrolled in college',
-    //     color: '#e6e6e6',
-    //     column: 3,
-    //     offset: -30
-    // },
-    // {
-    //     id: 'Bachelor\'s degree',
-    //     color: '#F9E79F',
-    //     column: 4,
-    //     offset: 50
-    // },
-    // {
-    //     id: 'Associate degree',
-    //     color: '#009c00',
-    //     column: 4,
-    //     offset: 50
-    // },
-    // {
-    //     id: 'Community college certificate',
-    //     color: '#1a8dff',
-    //     column: 4,
-    //     offset: 50
-    // },
-    // {
-    //     id: 'Attended, did not yet earn degree',
-    //     color: '#009c00',
-    //     column: 4,
-    //     offset: 50
-    // }
+    {
+        id: 'Did Not Complete High School',
+        column: 2,
+        color: '#8cff74',
+      },
+    {
+        id: 'Four-year college',
+        column: 3,
+        color: '#ff8da1',
+      },
+    {
+        id: 'Two-year community college',
+        column: 3,
+        color: '#f4c0ff',
+      },
+    {
+        id: 'Never enrolled in college',
+        column: 3,
+        color: '#e6e6e6',
+      },
+    {
+        id: 'Bachelor\'s degree',
+        column: 4,
+        color: '#F9E79F',
+      },
+    {
+        id: 'Associate degree',
+        column: 4,
+        color: '#009c00'
+      },
+    {
+        id: 'Community college certificate',
+        column: 4,
+        color: '#1a8dff'
+      },
+    {
+        id: 'Attended, did not yet earn degree',
+        column: 4,
+        color: '#009c00'
+      }
 ];
 
 
 let sankeyChart = Highcharts.chart('sankey-chart', {
-    title: {
-        text: 'Highcharts Sankey Diagram'
-    },
-    chart: {
-        type: "sankey",
-        nodes: nodes,
-        allowOverlap: true,
-        colorByPoint: false,
-        showInLegend: true,
-        color: "#ffff00",
-    },
 
-    series: [{
-        name: 'All Regions',
-        keys: ['from', 'to', 'weight', 'color'],
-        stack: 0,
-        data: [
-            ['Ninth Grade', 'Completed High School', 84.14, 'orange'],
-            ['Ninth Grade', 'Did Not Complete High School', 15.86, 'orange'],
-            ['Completed High School', 'Four-year school', 36.00, 'orange'],
-            ['Completed High School', 'Two-year school', 28.00, 'orange'],
-            ['Completed High School', 'Never enrolled', 20.14, 'orange'],
-            ['Did Not Complete High School', 'Four-year school', 2.86, 'orange'],
-            ['Did Not Complete High School', 'Two-year school', 5.00, 'orange'],
-            ['Did Not Complete High School', 'Never enrolled', 8.00, 'orange'],
-            ['Four-year school', 'Bachelor\'s degree', 18.00, 'orange'],
-            ['Four-year school', 'Associate degree', 10.00, 'orange'],
-            ['Four-year school', 'Community college certificate', 6.00, 'orange'],
-            ['Four-year school', 'Never enrolled in college', 4.86, 'orange'],
-            ['Two-year school', 'Bachelor\'s degree', 9.00, 'orange'],
-            ['Two-year school', 'Associate degree', 9.00, 'orange'],
-            ['Two-year school', 'Community college certificate', 9.00, 'orange'],
-            ['Two-year school', 'Never enrolled in college', 6.00, 'orange'],
-            ['Never enrolled', 'Never enrolled in college', 28.14, 'orange'],
+  title: {
+    text: ''
+  },
 
-        ]
-    } ,
-        {
-            name: 'Sirrea Nevada',
-            keys: ['from', 'to', 'weight', 'color'],
-            stack: 1,
-            data: [
-                ['Ninth Grade', 'Completed High School', 78.24, 'green'],
-                ['Ninth Grade', 'Did Not Complete High School', 21.76, 'green'],
-                ['Completed High School', 'Four-year school', 30.7, 'green'],
-                ['Completed High School', 'Two-year school', 34.7, 'green'],
-                ['Completed High School', 'Never enrolled', 28.14, 'green'],
-                ['Did Not Complete High School', 'Four-year school', 2.86, 'green'],
-                ['Did Not Complete High School', 'Two-year school', 5.00, 'green'],
-                ['Did Not Complete High School', 'Never enrolled', 8.00, 'green'],
-                ['Four-year school', 'Bachelor\'s degree', 18.00, 'green'],
-                ['Four-year school', 'Associate degree', 10.00, 'green'],
-                ['Four-year school', 'Community college certificate', 6.00, 'green'],
-                ['Four-year school', 'Never enrolled in college', 4.86, 'green'],
-                ['Two-year school', 'Bachelor\'s degree', 9.00, 'green'],
-                ['Two-year school', 'Associate degree', 9.00, 'green'],
-                ['Two-year school', 'Community college certificate', 9.00, 'green'],
-                ['Two-year school', 'Never enrolled in college', 6.00, 'green'],
-                ['Never enrolled', 'Never enrolled in college', 28.14, 'green'],
-            ],
+  subtitle: {
+    text: '',
+  },
+
+  caption: {
+    text: 'Dateset information Time Period: 2013-2023',
+    allowHTML: true,
+  },
+
+  credits: {
+    enabled: false,
+  },
+
+  chart: {
+    type: "sankey",
+    nodes: nodes,
+    allowOverlap: true,
+    colorByPoint: false,
+    showInLegend: true,
+    alignThresholds: true,
+    displayErrors: false,
+    ignoreHiddenSeries: false,
+    backgroundColor: 'none',
+  },
+
+  yAxis: {
+    floor: 0,
+    ceiling: 100,
+  },
+
+  exporting: {
+    chartOptions: {
+      plotOptions: {
+        series: {
+          dataLabels: {
+            enabled: true
+          }
         }
-    ],
+      }
+    }
+  },
+
+  plotOptions: {
+    sankey: {
+      nodeAlignment: 'top',
+      nodePadding: 60,
+      nodeDistance: 60,
+      centerInCategory: true,
+      enableMouseTracking: true,
+      visible: false,
+      linkOpacity: 0.1,
+      dataLabels: {
+        enabled: false,
+      },
+      label: {
+        enabled: true,
+        useHTML: true,
+      },
+    },
+  },
+
+  series: [
+    {
+      name: 'All Regions',
+      keys: ['from', 'to', 'weight', 'color'],
+      index: 1,
+      stack: 1,
+      // linkOpacity: 0.5, if no active group, enable this .5.
+      visible: true, // always to true. the enableMouseTracking will determine if its "active".
+      enableMouseTracking: false, // defaults to true. if no active group, set to true.
+      dataLabels: {
+        enabled: true,
+      },
+      data: [
+        ['Ninth Grade', 'Completed High School', 84.14, 'lightgray'],
+        ['Ninth Grade', 'Did Not Complete High School', 15.86, 'lightgray'],
+        ['Completed High School', 'Four-year school', 36.00, 'lightgray'],
+        ['Completed High School', 'Two-year school', 28.00, 'lightgray'],
+        ['Completed High School', 'Never enrolled', 20.14, 'lightgray'],
+        ['Did Not Complete High School', 'Four-year school', 2.86, 'lightgray'],
+        ['Did Not Complete High School', 'Two-year school', 5.00, 'lightgray'],
+        ['Did Not Complete High School', 'Never enrolled', 8.00, 'lightgray'],
+        ['Four-year school', 'Bachelor\'s degree', 18.00, 'lightgray'],
+        ['Four-year school', 'Associate degree', 10.00, 'lightgray'],
+        ['Four-year school', 'Community college certificate', 6.00, 'lightgray'],
+        ['Four-year school', 'Never enrolled in college', 4.86, 'lightgray'],
+        ['Two-year school', 'Bachelor\'s degree', 9.00, 'lightgray'],
+        ['Two-year school', 'Associate degree', 9.00, 'lightgray'],
+        ['Two-year school', 'Community college certificate', 9.00, 'lightgray'],
+        ['Two-year school', 'Never enrolled in college', 6.00, 'lightgray'],
+        ['Never enrolled', 'Never enrolled in college', 28.14, 'lightgray'],
+      ]
+    },
+    {
+      name: 'Group 1',
+      keys: ['from', 'to', 'weight'],
+      index: 99,
+      stack: 2,
+      linkOpacity: 0.5, // defaults to 0.1. the active group needs to be set to 0.5
+      visible: true, // defaults to false. the active group needs to be set to true
+      enableMouseTracking: true, // defaults to false. the active group needs to be set to true
+      data: [
+        ['Ninth Grade', 'Completed High School', 54.14],
+        ['Ninth Grade', 'Did Not Complete High School', 45.86],
+        ['Completed High School', 'Four-year school', 26.00],
+        ['Completed High School', 'Two-year school', 18.00],
+        ['Completed High School', 'Never enrolled', 10.14],
+        ['Did Not Complete High School', 'Four-year school', 1.86],
+        ['Did Not Complete High School', 'Two-year school', 4.00],
+        ['Did Not Complete High School', 'Never enrolled', 7.00],
+        ['Four-year school', 'Bachelor\'s degree', 18.00],
+        ['Four-year school', 'Associate degree', 10.00],
+        ['Four-year school', 'Community college certificate', 6.00],
+        ['Four-year school', 'Never enrolled in college', 4.86],
+        ['Two-year school', 'Bachelor\'s degree', 9.00],
+        ['Two-year school', 'Associate degree', 9.00],
+        ['Two-year school', 'Community college certificate', 9.00],
+        ['Two-year school', 'Never enrolled in college', 6.00],
+        ['Never enrolled', 'Never enrolled in college', 28.14],
+      ],
+    },
+    {
+      name: 'Group 2',
+      keys: ['from', 'to', 'weight'],
+      index: 99,
+      stack: 3,
+      data: [
+        ['Ninth Grade', 'Completed High School', 23.24],
+        ['Ninth Grade', 'Did Not Complete High School', 16.76],
+        ['Completed High School', 'Four-year school', 12.7],
+        ['Completed High School', 'Two-year school', 34.7],
+        ['Completed High School', 'Never enrolled', 56.14],
+        ['Did Not Complete High School', 'Four-year school', 2.86],
+        ['Did Not Complete High School', 'Two-year school', 5.00],
+        ['Did Not Complete High School', 'Never enrolled', 8.00],
+        ['Four-year school', 'Bachelor\'s degree', 18.00],
+        ['Four-year school', 'Associate degree', 10.00],
+        ['Four-year school', 'Community college certificate', 6.00],
+        ['Four-year school', 'Never enrolled in college', 4.86],
+        ['Two-year school', 'Bachelor\'s degree', 9.00],
+        ['Two-year school', 'Associate degree', 9.00],
+        ['Two-year school', 'Community college certificate', 9.00],
+        ['Two-year school', 'Never enrolled in college', 6.00],
+        ['Never enrolled', 'Never enrolled in college', 28.14],
+      ],
+    },
+    {
+      name: 'Group 3',
+      keys: ['from', 'to', 'weight'],
+      index: 99,
+      stack: 4,
+      data: [
+        ['Ninth Grade', 'Completed High School', 23.24],
+        ['Ninth Grade', 'Did Not Complete High School', 16.76],
+        ['Completed High School', 'Four-year school', 12.7],
+        ['Completed High School', 'Two-year school', 34.7],
+        ['Completed High School', 'Never enrolled', 56.14],
+        ['Did Not Complete High School', 'Four-year school', 2.86],
+        ['Did Not Complete High School', 'Two-year school', 5.00],
+        ['Did Not Complete High School', 'Never enrolled', 8.00],
+        ['Four-year school', 'Bachelor\'s degree', 18.00],
+        ['Four-year school', 'Associate degree', 10.00],
+        ['Four-year school', 'Community college certificate', 6.00],
+        ['Four-year school', 'Never enrolled in college', 4.86],
+        ['Two-year school', 'Bachelor\'s degree', 9.00],
+        ['Two-year school', 'Associate degree', 9.00],
+        ['Two-year school', 'Community college certificate', 9.00],
+        ['Two-year school', 'Never enrolled in college', 6.00],
+        ['Never enrolled', 'Never enrolled in college', 28.14],
+      ],
+    }
+  ],
 });
 
 console.log(sankeyChart.series[0].name);
